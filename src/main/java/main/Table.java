98dcd2e5b61f663fa157e884c32d98ca203c084d
gettyreal <getty.fafano@gmail.com>
2024-12-23 19:00:51 +0100

    added table rules text

--- src/main/java/main/Table.java
+++ src/main/java/main/Table.java
@@ -15,6 +15,17 @@ public class Table implements Colors {
         //draws table dividing line
         g2.setColor(Colors.white);
         g2.setStroke(new BasicStroke(5, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-        g2.drawLine(gp.maxWidht / 2 - 100, gp.maxHeight / 2, gp.maxWidht / 2 + 100, gp.maxHeight / 2);
+        g2.drawLine(gp.maxWidht / 2 - 125, gp.maxHeight / 2, gp.maxWidht / 2 + 125, gp.maxHeight / 2);
+
+        //draws table rules text
+        g2.setFont(gp.courier16);
+        drawText(g2, "BlackJack pays 3 to 2", gp.maxWidht / 2, gp.maxHeight / 2 - 10);
+        drawText(g2, "Insurance pays 2 to 1", gp.maxWidht / 2, gp.maxHeight / 2 + 20);
+    }
+
+    public void drawText(Graphics2D g2, String text, int x, int y) {
+        int length = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+        x = x - length / 2;
+        g2.drawString(text, x, y);
     }
 }
